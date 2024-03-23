96b34cf96eb578d5dda52f60852887c107452e54
Pavan Rajyaguru <pavanrajyaguru1001@gmail.com>
2024-03-23 17:24:05 +0530

    Migrate before use

--- stockmarket/sm_app/models.py
+++ stockmarket/sm_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True,verbose_name="ID")
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=150)
+    gender = models.CharField(max_length=50)
+    
+class Watch_list(models.Model):
+    user_id = models.ForeignKey("User", verbose_name=("user_id"), on_delete=models.CASCADE)
+    index = models.CharField(max_length=255)
+    w_name = models.CharField(max_length=100)
+    date_modified = models.DateTimeField(auto_now=True)
--- stockmarket/sm_app/views.py
+++ stockmarket/sm_app/views.py
@@ -21,3 +21,88 @@ def register(request):
 def get_index_history(request):
     print("Called")
     return HttpResponse("Hello World")
+
+@csrf_exempt
+def register_action(request):
+    post_data = json.loads(request.body)
+    uname =  post_data["username"]
+    email = post_data["email"]
+    password = make_password(post_data["password"])
+    gender = post_data["gender"]
+    
+    user_obj = User.objects.filter(email = email).values()
+    if len(user_obj) > 0:
+        return HttpResponse("Email already exist!")
+    else :
+        user_obj = User(name = uname,email = email,password = password,gender = gender)
+        user_obj.save()
+        return HttpResponse("User saved successfully!")
+
+@csrf_exempt
+def auth_user(request):
+    post_data = json.loads(request.body)
+    email = post_data["email"]
+    password = post_data["password"]
+
+    if email != None and password != None:
+        user_obj = User.objects.filter(email = email).values()
+        print(user_obj[0],"user")
+        if len(user_obj) > 0 :
+            if check_password(password,user_obj[0]["password"]):
+                response = { "code":1,"msg":"Login Successfull" ,"data":user_obj[0]}
+                request.session["id"] = user_obj[0]["id"]
+                request.session["name"] = user_obj[0]["name"]
+                request.session["email"] = user_obj[0]["email"]
+                request.session["gender"] = user_obj[0]["gender"]
+            else : 
+                response = { "code":0,"msg":"Incorrect Email or password"}
+            return HttpResponse(json.dumps(response))
+        
+    return HttpResponse(json.dumps({ "code":0,"msg":"Email and Password not found" }))
+        
+def is_login(request):
+    
+    response = {
+        "code" : 0,
+        "data" : {}
+    }
+    
+    if "id" in request.session:
+        response["code"] = 1
+        response["data"]["id"] = request.session["id"]
+        response["data"]["name"] = request.session["name"]
+        response["data"]["email"] = request.session["email"]
+        response["data"]["gender"] = request.session["gender"]
+    
+    return HttpResponse(json.dumps(response))
+
+
+    
+def logout(request):
+    try:
+        del request.session['id']
+        del request.session['name']
+        del request.session['email']
+        del request.session['gender']
+    except KeyError:
+        pass
+    return HttpResponse("Logout Successfull")
+
+
+@csrf_exempt
+def get_indices(request):
+    positions = nsefetch('https://www.nseindia.com/api/equity-stockIndices?index=SECURITIES%20IN%20F%26O')
+    post_data = json.loads(request.body)
+    start = post_data.get("start",0)
+    end = post_data.get("end",10)
+    print(get_bhavcopy("04-06-2021"))
+    # events = nse_events()
+    # circular = nse_circular(mode="latest")
+    # quote = nsetools_get_quote("IBM")
+    
+    response = positions["data"][int(start):int(end)]
+    return HttpResponse(json.dumps(response))
+
+
+def get_running_status(request):
+    return HttpResponse(str(running_status()))
